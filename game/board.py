367590b43e4c6e2ec7a33ef9accc1499b7847f47
bHartiii <malibharti5@gmail.com>
2025-09-05 18:53:29 +0530

    ADD: Board & SnakeLadderGame class

---
+++ game/board.py
@@ -0,0 +1,47 @@
+class Board:
+    
+    def __init__(self):
+        self.ladder_positions_map = {
+            7:  33,
+            37: 85,
+            51: 72,
+            78: 98
+        }
+        self.snake_positions_map = {
+            36: 19,
+            65: 35,
+            87: 32,
+            91: 53
+        }
+
+
+    def user_input(self):
+        while True:
+            try:
+                roll = int(input("Press Enter to roll the dice..."))
+                if roll in range(1, 7):
+                    return roll
+                else:
+                    print("Invalid input. Please press Enter to roll the dice.")
+            except ValueError:
+                print("Invalid input. Please press Enter to roll the dice.")
+
+
+    def update_player_position(self, current_position, roll):
+        ## Check if the move is valid
+        if current_position + roll > 100:
+            print(f"player rolled {roll} but cannot move from {current_position} to {current_position + roll} (exceeds 100).")
+            return current_position
+
+        ## check if the new position is a ladder or snake
+        new_position = current_position + roll
+
+        if new_position in self.ladder_positions_map:
+            print(f"player climbed a ladder from {new_position} to {self.ladder_positions_map[new_position]}")
+            new_position = self.ladder_positions_map[new_position]
+        elif new_position in self.snake_positions_map:
+            print(f"Player got bitten by a snake from {new_position} to {self.snake_positions_map[new_position]}")
+            new_position = self.snake_positions_map[new_position]
+        
+        return new_position
+    
---
+++ game/game.py
@@ -0,0 +1,38 @@
+from player import Player
+from board import Board
+
+class SnakeLadderGame:
+    def __init__(self):
+        self.players = []
+        self.is_over = False
+
+    def setup(self):
+        print(f"Starting game: Snake and Ladders")
+
+        ## Board initialization
+        self.board = Board()
+
+        ## Player initialization
+        self.players.append(Player("Alice"))
+
+    def play(self):
+        for player in self.players:
+            roll = self.board.user_input()
+            new_position = self.board.update_player_position(player.current_position, roll)
+            player.move(new_position)
+            if player.current_position == 100:
+                print(f"{player.name} has won the game!")
+                self.is_game_over = True
+                break
+
+    def end(self):
+        print("Game has ended.")
+
+
+if __name__ == "__main__":
+    game = SnakeLadderGame()
+    game.setup()
+    while not game.is_over:
+        game.play()
+
+    game.end()
