04e15b905d9a7d1c6324780b7e2b42ea9b3e66cb
bHartiii <malibharti5@gmail.com>
2025-09-08 02:33:27 +0530

    REFACTOR: to use config file in player class

--- game/player.py
+++ game/player.py
@@ -1,9 +1,9 @@
+import config
 class Player:
     def __init__(self, name):
         self.name = name
-        self.current_position = 0
+        self.current_position = config.starting_position
 
     def move(self, new_position):
         self.current_position = new_position
         print(f"{self.name} moved to position {self.current_position}")
-
